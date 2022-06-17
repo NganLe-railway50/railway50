3def711a872072988126bd91b32861772350fd9b
NganLe-railway50 <sweet.love0491@gmail.com>
2022-06-18 01:03:21 +0900

    update assignment

--- SQL/Assignment/Testing_System_Assignment_1.sql
+++ SQL/Assignment/Testing_System_Assignment_1.sql
@@ -7,70 +7,70 @@ DepartmentName		VARCHAR(50)
 );
 
 CREATE TABLE Position (
-PositionID			INT,
+PositionID		INT,
 PositionName		VARCHAR(30)
 );
 
 CREATE TABLE Account (
-AccountID			INT,
-Email				VARCHAR(50),
-Username			VARCHAR(50),
-FullName			VARCHAR(100),
+AccountID		INT,
+Email			VARCHAR(50),
+Username		VARCHAR(50),
+FullName		VARCHAR(100),
 DepartmentID		INT,
-PositionID			INT,
-CreateDate			DATE
+PositionID		INT,
+CreateDate		DATE
 );
 
 CREATE TABLE Nhom (
-GroupID				INT,
-GroupName			VARCHAR(30),
-CreatorID			INT,
-CreateDate			DATE
+GroupID			INT,
+GroupName		VARCHAR(30),
+CreatorID		INT,
+CreateDate		DATE
 );
 
 CREATE TABLE GroupAccount (
-GroupID				INT,
-AccountID			INT,
-JoinDate			DATE
+GroupID			INT,
+AccountID		INT,
+JoinDate		DATE
 );
 
 CREATE TABLE TypeQuestion (
-TypeID				INT,
-TypeName			VARCHAR(30)
+TypeID			INT,
+TypeName		VARCHAR(30)
 );
 
 CREATE TABLE CategoryQuestion (
-CategoryID			INT,
+CategoryID		INT,
 CategoryName		VARCHAR(30)
 );
 
 CREATE TABLE Question (
-QuestionID			INT,
-Content				VARCHAR(200),
-CategoryID			INT,
-TypeID				INT,
-CreatorID			INT,
-CreateDate			DATE
+QuestionID		INT,
+Content			VARCHAR(200),
+CategoryID		INT,
+TypeID			INT,
+CreatorID		INT,
+CreateDate		DATE
 );
 
 CREATE TABLE Answer (
-AnswerID			INT,
-Content				VARCHAR(200),
-QuestionID			INT,
-IsCorrect			VARCHAR(10)
+AnswerID		INT,
+Content			VARCHAR(200),
+QuestionID		INT,
+IsCorrect		VARCHAR(10)
 );
 
 CREATE TABLE Exam (
-ExamID				INT,
+ExamID			INT,
 Code_MaDeThi		INT,
-Title				VARCHAR(50),
-CategoryID			INT,
-Duration			VARCHAR(30),
-CreatorID			INT,
-CreateDate			DATE
+Title			VARCHAR(50),
+CategoryID		INT,
+Duration		VARCHAR(30),
+CreatorID		INT,
+CreateDate		DATE
 );
 
 CREATE TABLE ExamQuestion (
-ExamID				INT,
-QuestionID			INT
+ExamID			INT,
+QuestionID		INT
 );
